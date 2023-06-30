4c7ae08e894a26ebf37863ebf48cf8ae0d406db6
Sudheer Kothari <126937415+sudheerkothari@users.noreply.github.com>
2023-06-30 09:04:30 +0500

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,16 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '20b2b99892msh8b8448cfbfa2fe1p19d24cjsn957514d5f002',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}2132
